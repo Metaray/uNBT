72150e34ea957c6812c2ad600dd0af77ea1081b2
Metaray <metaray21@gmail.com>
2020-01-22 10:53:31 +0300

    Added package setup script.

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='uNBT',
+    version='1.0',
+    description='Simple NBT manipulation library',
+    packages=['uNBT'],
+    python_requires='>=3.5',
+)
