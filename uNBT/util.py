687e370ebcd0a2a25f5ad2f2e3949e7a9bff2080
Metaray <metaray21@gmail.com>
2021-01-10 11:15:01 +0300

    Remove fancy_tag_format from utilities
    Minor improvements to output of print module command.

--- uNBT/util.py
+++ uNBT/util.py
@@ -1,37 +1,13 @@
-from .nbt import TagByteArray, TagIntArray, TagList, TagCompound, TagString
 import os
 import re
 from collections import namedtuple
 
 __all__ = [
-	'fancy_tag_format',
 	'RegionFileInfo',
 	'region_pos_from_path',
 	'enumerate_region_files',
 	'enumerate_world',
 ]
-
-
-def fancy_tag_format(tag, indent='  ', level=0):
-	out = ''
-	tag_name = tag.__class__.__name__
-	if tag.tagid in (TagByteArray.tagid, TagIntArray.tagid):
-		out += '{} {}'.format(tag_name, tag._value.tolist())
-	elif tag.tagid == TagList.tagid:
-		out += '{} [\n'.format(tag_name)
-		for x in tag._value:
-			out += '{}{}\n'.format(indent * (level + 1), fancy_tag_format(x, indent, level + 1))
-		out += indent * level + ']'
-	elif tag.tagid == TagCompound.tagid:
-		out += 'TagCompound {\n'
-		for name in tag._value:
-			out += '{}{}: {}\n'.format(indent * (level + 1), name, fancy_tag_format(tag._value[name], indent, level + 1))
-		out += indent * level + '}'
-	elif tag.tagid == TagString.tagid:
-		out += repr(tag)
-	else:
-		out += '{}({})'.format(tag_name, tag._value)
-	return out
 
 
 RegionFileInfo = namedtuple('RegionFileInfo', ['path', 'x', 'z'])
