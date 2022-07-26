d9aa304d9fcb8837691656c15513a501e4b584a8
Metaray <metaray21@gmail.com>
2022-07-26 13:08:58 +0300

    Add deserialization (read) tests

--- tests/test_read_write.py
+++ tests/test_read_write.py
@@ -7,11 +7,15 @@ import sys
 class TestReadWrite(unittest.TestCase):
     def test_save_load_cycle(self):
         self.run_save_load_for('bigtest.nbt')
-    
+        self.run_save_load_for('bigtest2.nbt')
+
+
     def run_save_load_for(self, filename):
         test_file = BytesIO(read_test_data(filename))
         tag, name = nbt.read_nbt_file(test_file, with_name=True)
+        self.assertEqual(test_file.tell(), len(test_file.getvalue()), 'Unread data remaining')
         self.check_save_load(tag, name)
+
 
     def check_save_load(self, tag, name):
         # Save
@@ -31,10 +35,15 @@ class TestReadWrite(unittest.TestCase):
     # This test guarantees we don't lose any data when reading
     @unittest.skipIf(sys.version_info < (3, 7), 'Not guaranteed on this python version')
     def test_exact_resave(self):
-        source_file = BytesIO(read_test_data('bigtest.nbt'))
+        self.run_exact_resave_for('bigtest.nbt')
+        self.run_exact_resave_for('bigtest2.nbt')
+
+
+    def run_exact_resave_for(self, filename):
+        source_file = BytesIO(read_test_data(filename))
         tag, name = nbt.read_nbt_file(source_file, with_name=True)
         
         mock_file = BytesIO()
         nbt.write_nbt_file(mock_file, tag, root_name=name, compress=False)
 
-        self.assertEqual(mock_file.getbuffer(), source_file.getbuffer())
+        self.assertEqual(mock_file.getvalue(), source_file.getvalue(), 'Inexact NBT file resave')
---
+++ tests/test_serialization.py
@@ -0,0 +1,96 @@
+import unittest
+import uNBT as nbt
+from io import BytesIO
+from .utils import read_test_data
+import math
+import struct
+
+
+def round_f32(x):
+    return struct.unpack('f', struct.pack('f', x))[0]
+
+
+class TestSerialization(unittest.TestCase):
+    def test_bigtest(self):
+        ref = self.create_bigtest()
+        tag = nbt.read_nbt_file(BytesIO(read_test_data('bigtest.nbt')))
+        self.assertEqual(tag, ref)
+
+    def test_bigtest2(self):
+        ref = self.create_bigtest2()
+        tag = nbt.read_nbt_file(BytesIO(read_test_data('bigtest2.nbt')))
+        self.assertEqual(tag, ref)
+
+
+    @staticmethod
+    def create_bigtest():
+        return nbt.TagCompound({
+            'longTest': nbt.TagLong(9223372036854775807),
+            'shortTest': nbt.TagShort(32767),
+            'stringTest': nbt.TagString('HELLO WORLD THIS IS A TEST STRING ÅÄÖ!'),
+            'floatTest': nbt.TagFloat(0.4982314705848694),
+            'intTest': nbt.TagInt(2147483647),
+            'nested compound test': nbt.TagCompound({
+                'ham': nbt.TagCompound({
+                    'name': nbt.TagString('Hampus'),
+                    'value': nbt.TagFloat(0.75)
+                }),
+                'egg': nbt.TagCompound({
+                    'name': nbt.TagString('Eggbert'),
+                    'value': nbt.TagFloat(0.5)
+                })
+            }),
+            'listTest (long)': nbt.TagList(nbt.TagLong, [nbt.TagLong(11), nbt.TagLong(12), nbt.TagLong(13), nbt.TagLong(14), nbt.TagLong(15)]),
+            'listTest (compound)': nbt.TagList(nbt.TagCompound, [
+                nbt.TagCompound({
+                    'name': nbt.TagString('Compound tag #0'),
+                    'created-on': nbt.TagLong(1264099775885)
+                }),
+                nbt.TagCompound({
+                    'name': nbt.TagString('Compound tag #1'),
+                    'created-on': nbt.TagLong(1264099775885)
+                })
+            ]),
+            'byteTest': nbt.TagByte(127),
+            'byteArrayTest (the first 1000 values of (n*n*255+n*7)%100, starting with n=0 (0, 62, 34, 16, 8, ...))': nbt.TagByteArray([
+                (n*n*255+n*7)%100 for n in range(1000)
+            ]),
+            'doubleTest': nbt.TagDouble(0.4931287132182315),
+        })
+
+
+    @staticmethod
+    def create_bigtest2():
+        def make_rec_list(depth):
+            if depth <= 1:
+                return nbt.TagList(nbt.TagList)
+            return nbt.TagList(nbt.TagList, [
+                make_rec_list(1),
+                make_rec_list(depth - 1),
+            ])
+
+        def make_rec_compound(depth):
+            if depth <= 1:
+                return nbt.TagCompound({
+                    "Begin": nbt.TagByte(-128),
+                    "end.": nbt.TagShort(2**15 - 1),
+                })
+            return nbt.TagCompound({
+                'A': make_rec_compound(1),
+                'B': make_rec_compound(depth - 1),
+            })
+
+        return nbt.TagCompound({
+            'byte': nbt.TagByte(42),
+            'short': nbt.TagShort(12345),
+            'int': nbt.TagInt(1234567890),
+            'long': nbt.TagLong(1234567890123456789),
+            'float': nbt.TagFloat(round_f32((1 + 5**0.5) / 2)),
+            'double': nbt.TagDouble(math.pi),
+            'byte array': nbt.TagByteArray([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, -122, -23, 121, 98, -37, 61, 24, 85]),
+            'string': nbt.TagString('Lorem ipsum\n\thello\tworld'),
+            'tag list': make_rec_list(16),
+            'compound': make_rec_compound(16),
+            'int array': nbt.TagIntArray([x * 111111111 for x in range(-9, 10)]),
+            'long array': nbt.TagLongArray([x * 1111111111111111111 for x in range(-8, 9)]),
+        })
