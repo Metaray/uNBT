51497ab9925a6d342a4cdd8ce0e0696aff0f9b22
Metaray <metaray21@gmail.com>
2023-02-12 12:45:27 +0300

    Fix unittest for older python versions

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,9 +3,9 @@ from pathlib import Path
 def load_tests(loader, suite, pattern):
     this_dir = Path(__file__).parent
     package_tests = loader.discover(
-        start_dir=this_dir,
+        start_dir=str(this_dir),
         pattern='test_*.py',
-        top_level_dir=this_dir.parent,
+        top_level_dir=str(this_dir.parent),
     )
     suite.addTests(package_tests)
     return suite
